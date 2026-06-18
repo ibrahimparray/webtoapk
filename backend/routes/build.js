9d443ef3ea72f7c23f5fa06c185973c52fe1528f
ibrahimparray <ibrahimparray066@gmail.com>
2026-06-18 17:45:51 +0530

    Delete backend directory

--- backend/routes/build.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const upload = require('../middleware/upload');
-const { handleBuild } = require('../controllers/buildController');
-
-const router = Router();
-
-router.post('/build', upload.single('icon'), handleBuild);
-
-module.exports = router;
